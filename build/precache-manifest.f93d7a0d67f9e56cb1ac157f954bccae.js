44477a4a2588cae729ae38e014f7ad58e7669a25
zjimf <jmjmjim1101@gmail.com>
2022-11-12 22:04:26 +0800

    Update

---
+++ build/precache-manifest.f93d7a0d67f9e56cb1ac157f954bccae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c7060154efed7f90d06e307a147321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "041206b06e3939822a8f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d2eb857e002331230a4",
+    "url": "/static/js/2.4c5e9dae.chunk.js"
+  },
+  {
+    "revision": "6c2b76e67dc1282ff84dbe1bf5de9598",
+    "url": "/static/js/2.4c5e9dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041206b06e3939822a8f",
+    "url": "/static/js/main.559c73bc.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
